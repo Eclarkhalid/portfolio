7ebf2694769fa14f396cd5c51aa2a5b80a4d0489
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-14 11:50:56 +0300

    Updating Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
   return <>
     <footer className="flex justify-between w-full items-center pt-2 mb-8 md:mb-2">
       <div className="links flex gap-4 text-xl">
-        <Link to={'/'} className='text-gray-600 hover:text-gray-900'>
+        <Link to={'https://twitter.com/eclarkhalid'} className='text-gray-600 hover:text-gray-900'>
           <FaSquareXTwitter />
         </Link>
-        <Link to={'/'} className='text-gray-600 hover:text-gray-900'>
+        <Link to={'https://www.youtube.com/@eclarkhalid/videos'} className='text-gray-600 hover:text-gray-900'>
           <AiFillYoutube />
         </Link>
-        <Link to={'/'} className='text-gray-600 hover:text-gray-900'>
+        <Link to={'https://instagram.com/eclar_khalid?igshid=OGQ5ZDc2ODk2ZA=='} className='text-gray-600 hover:text-gray-900'>
           <AiFillInstagram />
         </Link>
 
-        <Link to={'/'} className='text-gray-600 hover:text-gray-900'>
+        <Link to={'https://discord.gg/RBG3BATb'} className='text-gray-600 hover:text-gray-900'>
           <BsDiscord />
         </Link>
 
