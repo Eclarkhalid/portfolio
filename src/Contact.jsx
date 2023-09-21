c2fac634024a2097d29ec71844ecb2edf96c641e
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-21 09:58:27 +0300

    Updating the contact section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -30,8 +30,9 @@ export const Contact = () => {
 
   return (
 
-    <div className=" w-full my-12 link_card">
-      <form ref={form} onSubmit={sendEmail} className="space-y-5">
+    <div className=" w-full my-12 ">
+      <h2 className="desc mb-3 font-semibold">Leave me a message. Let me know your feedback.</h2>
+      <form ref={form} onSubmit={sendEmail} className="space-y-5 link_card">
         <div>
           <label for="example1" className="mb-2 block font-medium text-gray-700 text-xl">Your Name</label>
           <input type="name" id="example1" className="w-full px-4 py-2 mt-1 bg-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Name" name="user_name" />
@@ -49,7 +50,7 @@ export const Contact = () => {
           </div>
         </div>
 
-        <button type="submit" value="Send" className="rounded-lg border border-primary-500 bg-primary-500 px-5 py-2.5 text-center font-semibold text-slate-900 border-blue-300 shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300">Submit</button>
+        <button type="submit" value="Send" className="rounded-lg border border-primary-500 bg-primary-500 px-5 py-2.5 text-center font-semibold text-slate-900 border-blue-300 shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300">Send Message</button>
       </form>
     </div>
 
