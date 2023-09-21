861b5dac760c933a16014ffcf45b169b64687e37
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-21 10:46:05 +0300

    Updating the contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -63,8 +63,8 @@ export const Contact = () => {
           <textarea
             id="message"
             className="w-full px-4 py-2 mt-1 bg-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            rows="3"
-            placeholder="Leave a message"
+            rows="5"
+            placeholder="Type Message here ..."
             name="message"
           ></textarea>
         </div>
