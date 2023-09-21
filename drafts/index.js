d5a9f94393685c92524555dfe7441dcdcfbc0f90
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-21 10:58:30 +0300

    Adding drafts

--- drafts/index.js
+++ drafts/index.js
@@ -1,5 +1,12 @@
 export const draftLinks = [
 
-  
+  {
+    title: "Recipe Rise",
+    heading: "This app works but have to fix some bugs.",
+    url: "https://recipe-first-site.vercel.app/",
+    year: 'MERN Stack',
+    code: "https://github.com/Eclarkhalid/recipe-first-site",
+    avatar: "R.R"
+  },
 
 ]
