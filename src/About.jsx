d80a99fc74767aac325f5838504b8e37f4c132a4
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-25 11:20:29 +0300

    Updating about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,28 +1,38 @@
+import React from 'react';
+
 function About() {
-  return <>
+  return (
     <section className="mt-16 w-full max-w-4xl">
       <div className="flex flex-col w-full desc">
-        <p>
-          Hi! I am a <span className="text-blue-400">web developer</span> based in <span className="text-blue-400">Nairobi, Kenya</span>, with a strong passion for creating <span className="text-blue-400">web applications</span>. I hold a <span className="text-blue-400">BSc in Business Information Technology</span> from <span className="text-blue-400">Mount Kenya University</span>, where I honed my skills in software development and technology management.
-        </p>
-        <br className="my-3" />
-        <p>
-          Throughout my educational journey, I have not only focused on academics but also actively worked on personal projects and collaborated with clients to develop custom <span className="text-blue-400">web solutions</span>. These experiences have given me hands-on expertise in <span className="text-blue-400">front-end and back-end development</span>, as well as a deep understanding of <span className="text-blue-400">user experience</span> and <span className="text-blue-400">design principles</span>.
-        </p>
-        <br className="my-3" />
-        <p>
-          In addition to my development work, I have cultivated a presence on <a href="https://www.youtube.com/@eclarkhalid/videos" target="_blank" rel="noopener noreferrer" className="text-blue-900 underline">YouTube</a>, where I share my knowledge and teach <span className="text-blue-400">programming</span> to a growing audience.
-        </p>
-        <br className="my-3" />
-        <p>
-          You can gain further insights into my background and interests through my <span className="text-blue-400">full resume</span>, <span className="text-blue-400">projects</span>, and various <span className="text-blue-400">social media profiles</span>.
+        <p className="text-lg font-semibold mb-3">
+          Hi, I'm a web developer based in Nairobi, Kenya, with a passion for creating web applications. I hold a BSc in Business Information Technology from Mount Kenya University, specializing in:
         </p>
 
+        <ul className="list-disc ml-6 text-lg mb-3">
+          <li><span className="font-semibold text-blue-600">React:</span> Building dynamic and interactive user interfaces.</li>
+          <li><span className="font-semibold text-blue-600">Next.js:</span> Creating server-rendered React apps for performance and SEO.</li>
+          <li><span className="font-semibold text-blue-600">HTML/CSS:</span> Crafting visually appealing and responsive web designs.</li>
+          <li><span className="font-semibold text-blue-600">JavaScript:</span> Adding interactivity and functionality to web apps.</li>
+          <li><span className="font-semibold text-blue-600">MERN Technologies:</span> Developing full-stack web applications.</li>
+          <li><span className="font-semibold text-blue-600">Git:</span> Ensuring efficient code management and collaboration.</li>
+        </ul>
+
+        <p className="text-lg mb-3 font-medium">
+          On a personal note, I'm known for my dedication, problem-solving skills, and an unwavering commitment to quality. I thrive in collaborative environments and enjoy tackling complex challenges.
+        </p>
+
+        <p className="text-lg mb-3 font-medium">
+          In addition to my development work, I share my knowledge and teach programming on <a href="https://www.youtube.com/@eclarkhalid/videos" target="_blank" rel="noopener noreferrer" className="text-blue-900 underline">YouTube</a>. I'm committed to staying up-to-date with the latest trends in web development and am eager to collaborate on exciting projects. Feel free to reach out if you'd like to work together!
+        </p>
+        
+        <p className="text-lg mb-3 font-medium">
+          View the <a href="/projects" className="text-blue-900 underline">Projects section</a> to see my work.
+        </p>
+        
         <hr className="my-8 h-px border-0 bg-gray-300" />
       </div>
-
     </section>
-  </>
+  );
 }
 
 export default About;
